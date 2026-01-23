d8756bd1d376015e18f23c26ca6ffe688a877b3d
sushmita singh <payalsingh9602@gmail.com>
2026-01-23 23:53:58 +0530

    lets make something different

---
+++ reactrouter/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Home from "./components/Home/Home";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+import "./index.css";
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Home />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
